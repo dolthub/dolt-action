06ffea961d67ca4e2edf9267064ee5d5b073bc66
Max Hoffman <max@dolthub.com>
2021-05-25 16:24:38 -0700

    e2e table load

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 # Container image that runs your code
 FROM alpine:3.10
 
+RUN apk add --no-cache bash curl jq git \
+    && curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh | bash \
+    && mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2 \
+    && dolt config --global --add metrics.host eventsapi.awsdev.ld-corp.com \
+    && dolt config --global --add metrics.port 443
+    #&& dolt config --global --add user.email bojack@horseman.com \
+    #&& dolt config --global --add user.name "Bojack Horseman" \
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
+# Code file to execute when the docker container starts up (`entrypoint.sh`
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,73 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+set -eox pipefail
+
+doltdb=$GITHUB_WORKSPACE/doltdb
+
+_main() {
+    _configure
+    _clone
+    _run
+    _commit
+    _tag
+    _push
+}
+
+_configure() {
+    dolt config --global --add user.name "$INPUT_COMMIT_USER_NAME"
+    dolt config --global --add user.email "$INPUT_COMMIT_USER_EMAIL"
+
+    # DoltHub password
+    if [ ! -z "$INPUT_DOLTHUB_CREDENTIAL" ]; then
+        echo "$INPUT_DOLTHUB_CREDENTIAL" | dolt creds import
+    fi
+
+    # AWS
+    if [ ! -z "$INPUT_AWS_CREDENTIALS" ]; then
+        echo "aws creds not properly handled yet"
+    fi
+
+    # GCP
+    if [ ! -z "$INPUT_GCP_CREDENTIALS" ]; then
+        echo "gcp creds not properly handled yet"
+    fi
+}
+
+_clone () {
+    dolt clone $INPUT_REMOTE -b $INPUT_BRANCH $doltdb || dolt clone $INPUT_REMOTE -b master $doltdb
+    cd $doltdb
+
+    if [ "$(dolt sql -q "select active_branch()" -r csv | head -2 | tail -1)" -ne "$INPUT_BRANCH" ]; then
+        dolt checkout -b $branch
+    fi
+}
+
+_run() {
+    /bin/bash -c "$INPUT_RUN"
+}
+
+_commit() {
+    dolt add .
+    dolt commit -m "$INPUT_MESSAGE"
+
+    if [ -n "$(dolt diff)" ]; then
+      echo "dolt status is not clean"
+      exit 1
+    fi
+
+    head="$(dolt sql -q "select hashof('HEAD')" -r csv | head -2 | tail -1)"
+    echo "::set-output name=commit::$head"
+}
+
+# TODO tagging
+_tag() {
+    echo "todo: tagging"
+}
+
+_push() {
+    if [ "$INPUT_PUSH" = true ]; then
+        dolt push origin "$INPUT_BRANCH"
+    fi
+}
+
+_main
