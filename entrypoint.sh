af16a6dde0c9e08c9efa2cd2c1edcc8728ac3742
Max Hoffman <max@dolthub.com>
2021-06-03 13:10:59 -0700

    Wrap string variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eox pipefail
 
-doltdb=$GITHUB_WORKSPACE/doltdb
+doltdb="${GITHUB_WORKSPACE}/doltdb"
 
 _main() {
     _configure
@@ -14,45 +14,47 @@ _main() {
 }
 
 _configure() {
-    dolt config --global --add user.name "$INPUT_COMMIT_USER_NAME"
-    dolt config --global --add user.email "$INPUT_COMMIT_USER_EMAIL"
+    dolt config --global --add user.name "${INPUT_COMMIT_USER_NAME}"
+    dolt config --global --add user.email "${INPUT_COMMIT_USER_EMAIL}"
 
     # DoltHub password
-    if [ ! -z "$INPUT_DOLTHUB_CREDENTIAL" ]; then
-        echo "$INPUT_DOLTHUB_CREDENTIAL" | dolt creds import
+    if [ ! -z "${INPUT_DOLTHUB_CREDENTIAL}" ]; then
+        echo "${INPUT_DOLTHUB_CREDENTIAL}" | dolt creds import
     fi
 
     # AWS
-    if [ ! -z "$INPUT_AWS_CREDENTIALS" ]; then
+    if [ ! -z "${INPUT_AWS_CREDENTIALS}" ]; then
         echo "aws creds not properly handled yet"
     fi
 
     # GCP
-    if [ ! -z "$INPUT_GCP_CREDENTIALS" ]; then
+    if [ ! -z "${INPUT_GCP_CREDENTIALS}" ]; then
         echo "gcp creds not properly handled yet"
     fi
 }
 
 _clone () {
-    dolt clone $INPUT_REMOTE -b $INPUT_BRANCH $doltdb || dolt clone $INPUT_REMOTE -b master $doltdb
-    cd $doltdb
+    dolt clone "${INPUT_REMOTE}" -b "${INPUT_BRANCH}" "${doltdb}" \
+        || dolt clone "${INPUT_REMOTE}" -b master "${doltdb}"
+    cd "${doltdb}"
 
-    if [ "$(dolt sql -q "select active_branch()" -r csv | head -2 | tail -1)" -ne "$INPUT_BRANCH" ]; then
-        dolt checkout -b $branch
+    current_branch="$(dolt sql -q "select active_branch()" -r csv | head -2 | tail -1)"
+    if [ "${current_branch}" -ne "${INPUT_BRANCH}" ]; then
+        dolt checkout -b "${INPUT_BRANCH}"
     fi
 }
 
 _run() {
-    /bin/bash -xc "$INPUT_RUN"
+    /bin/bash -xc "${INPUT_RUN}"
 }
 
 _commit() {
     dolt add .
     status="$(dolt sql -q "select * from dolt_status where staged = true limit 1" -r csv | wc -l)"
-    if [ "$status" -ge 2 ]; then
-        dolt commit -m "$INPUT_MESSAGE"
+    if [ "${status}" -.github/workflows/test_plugin.ymlge 2 ]; then
+        dolt commit -m "${INPUT_MESSAGE}"
         head="$(dolt sql -q "select hashof('HEAD')" -r csv | head -2 | tail -1)"
-        echo "::set-output name=commit::$head"
+        echo "::set-output name=commit::${head}"
     else
       echo "dolt status is clean"
     fi
@@ -64,8 +66,8 @@ _tag() {
 }
 
 _push() {
-    if [ "$INPUT_PUSH" = true ]; then
-        dolt push origin "$INPUT_BRANCH"
+    if [ "${INPUT_PUSH}" = true ]; then
+        dolt push origin "${INPUT_BRANCH}"
     fi
 }
 
