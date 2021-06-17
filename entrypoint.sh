7803cb71ce0b5901f4684fb7f03ff8708749d478
Max Hoffman <max@dolthub.com>
2021-06-17 15:50:37 -0700

    Logging changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,9 @@ _main() {
 
 _configure() {
     dolt config --global --add user.name "${INPUT_COMMIT_USER_NAME}"
-    dolt config --global --add user.email "${INPUT_COMMIT_USER_EMAIL}"
+    config --global --add user.email "${INPUT_COMMIT_USER_EMAIL}"
 
-    # DoltHub password
+    # DoltHub JWT
     if [ ! -z "${INPUT_DOLTHUB_CREDENTIAL}" ]; then
         echo "${INPUT_DOLTHUB_CREDENTIAL}" | dolt creds import
         echo "Authenticated DoltHub credentials"
@@ -28,9 +28,8 @@ _configure() {
 
     # AWS credentials -- set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION
 
-    # GCP
+    # GCP Service Account Credentials
     if [ ! -z "${INPUT_GOOGLE_CREDENTIAL}" ]; then
-        #echo "${INPUT_GCP_CREDENTIALS}" | gcloud auth activate-service-account --key-file /dev/stdin
         echo "${INPUT_GOOGLE_CREDENTIAL}" >> /tmp/gcp_creds.json
         export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcp_creds.json
         echo "Autheniticated gcloud credentials"
@@ -38,7 +37,6 @@ _configure() {
 }
 
 _clone () {
-    echo "Cloning repo: ${INPUT_REMOTE}"
     dolt clone "${INPUT_REMOTE}" -b "${INPUT_BRANCH}" "${doltdb}" \
         || dolt clone "${INPUT_REMOTE}" -b master "${doltdb}"
     cd "${doltdb}"
@@ -57,19 +55,20 @@ _before() {
 _commit() {
     if [ -n "${INPUT_COMMIT_MESSAGE}" ]; then
         dolt add .
+        dolt status
         status="$(dolt sql -q "select * from dolt_status where staged = true limit 1" -r csv | wc -l)"
         if [ "${status}" -ge 2 ]; then
+            echo "committing changes"
             dolt commit -m "${INPUT_COMMIT_MESSAGE}"
             head="$(dolt sql -q "select hashof('HEAD')" -r csv | head -2 | tail -1)"
             echo "::set-output name=commit::${head}"
-        else
-          echo "dolt status is clean"
         fi
     fi
 }
 
 _tag() {
     if [ -n "${INPUT_TAG_NAME}" ]; then
+        echo "dolt tag configuration: name - ${INPUT_TAG_NAME}, message - ${INPUT_TAG_MESSAGE}, ref - ${INPUT_TAG_REF}"
         if [ -n "${INPUT_TAG_MESSAGE}" ]; then
             dolt tag -m "${INPUT_TAG_MESSAGE}"  "${INPUT_TAG_NAME}" "${INPUT_TAG_REF}"
         else
